101cb1b0db14a6e66008dd4904ae786ec0c93fa3
Corentin Delcourt <codl@codl.fr>
2013-04-27 15:05:27 +0200

    initial html

---
+++ swim.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var nojs = document.getElementById("nojs");
+    nojs.parentElement.removeChild(nojs);
+    var canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    // do shit
+}
