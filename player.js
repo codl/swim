a69954d8131929b6ba69d6471ac7ab4d486d73e8
Corentin Delcourt <codl@codl.fr>
2013-04-27 18:26:00 +0200

    better player, test graphics

--- player.js
+++ player.js
@@ -5,48 +5,64 @@ var player = {
     yv: 0,
     xa: 0, // xacceleration i.e. how much we change xv every frame
     ya: 0,
-    angle: 0,
+    directions: {
+        left: false,
+        up: false,
+        right: false,
+        down: false
+    },
     step: function(){
+        with(player.directions){
+            //console.log(player.directions);
+            player.xa = left? (right? 0 : -0.3) : (right? 0.3 : 0)
+            player.ya = up? (down? 0 : -0.3) : (down? 0.3 : 0)
+        }
         player.x += player.xv;
         player.y += player.yv;
         player.xv += player.xa;
         player.yv += player.ya;
         // friction
-        player.xv *= 0.9;
-        player.yv *= 0.9;
+        player.xv *= 0.95;
+        player.yv *= 0.95;
         // gravity
-        player.yv += 20 / Math.abs(player.xv);
+        player.yv += 0.02 / (1 + Math.abs(player.xv));
         // note : inversely proportional to horizontal movement to simulate "gliding"
+        context.fillRect(player.x-2, player.y-2, 5, 5);
+        //console.log(player.x, player.y, player.xv, player.yv, player.xa, player.ya);
     },
     keyup: function(e){
         switch(e.keyCode){
             case 37:
-            case 39:
-                player.xa = 0;
+                player.directions.left = false;
                 break;
             case 38:
+                player.directions.up = false;
+                break;
+            case 39:
+                player.directions.right = false;
+                break;
             case 40:
-                player.ya = 0;
+                player.directions.down = false;
+                break;
         }
     },
     keydown: function(e){
         switch(e.keyCode){
             case 37:
-                player.xa = -30;
+                player.directions.left = true;
                 break;
             case 38:
-                player.ya = -30;
+                player.directions.up = true;
                 break;
             case 39:
-                player.xa =  30;
+                player.directions.right = true;
                 break;
             case 40:
-                player.ya =  30;
+                player.directions.down = true;
                 break;
             default:
                 return;
         }
-        step();
         e.preventDefault();
     }
 }
