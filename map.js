3ca9fd61adb69d5d041632976139cd9d1cdd4878
Corentin Delcourt <codl@codl.fr>
2013-04-29 13:29:44 +0200

    background layer test

--- map.js
+++ map.js
@@ -11,24 +11,27 @@ var map = {
         var queue_right = [];
         var width = map.width;
         var img = map.context.getImageData(0, 0, map.width, map.height);
+        function v(x, y, z){
+            return map.perlin.noise(x/900, y/900, z?z:0) +
+                map.perlin.noise(x/250, y/250, z?z:0) +
+                map.perlin.noise(x/90, y/60, z?z:0)/9 +
+                map.perlin.noise(x/3, y/3, 0)/100 +
+                linear(y, 0, map.canvas.height, -1, 1)*2 +
+                Math.pow(linear(x, 0, map.width, -1, 1), 6)
+                - 0.1;
+        }
         function floodfill(x, y){
             if(x > rightmost){
                 rightmost = x;
             }
             img.data[(x + y * width) * 4] = 255; // transparent red, mark as seen
-            var v =
-                map.perlin.noise(x/900, y/900, 0)   +
-                map.perlin.noise(x/250, y/250, 0) +
-                map.perlin.noise(x/90, y/60, 0)/9 +
-                map.perlin.noise(x/3, y/3, 0)/100 +
-                linear(y, 0, map.canvas.height, -1, 1)*2 +
-                Math.pow(linear(x, 0, map.width, -1, 1), 6)
-                - 0.1;
-            if(v > 0){
+            var bg = v(x, y, 0.5) > -0.1;
+            var fg = v(x, y, 0) > 0;
+            if(bg || fg){
                 //img.data[(x + y * width) * 4] = 255; // already done up there ↑
                 img.data[(x + y * width) * 4 + 1] = 255;
                 img.data[(x + y * width) * 4 + 2] = 255;
-                img.data[(x + y * width) * 4 + 3] = 255;
+                img.data[(x + y * width) * 4 + 3] = fg?255:31;
                 if(y > 0                     && img.data[(x   + (y-1) * width) * 4] === 0) { queue.push      ([x  , y-1]); img.data[(x   + (y-1) * width) * 4] = 255; }
                 if(x < map.canvas.width - 1  && img.data[(x+1 + (y  ) * width) * 4] === 0) { queue_right.push([x+1, y  ]); img.data[(x+1 + (y  ) * width) * 4] = 255; }
                 if(x > 0                     && img.data[(x-1 + (y  ) * width) * 4] === 0) { queue.push      ([x-1, y  ]); img.data[(x-1 + (y  ) * width) * 4] = 255; }
@@ -77,8 +80,8 @@ var map = {
     },
     collide: function(x, y){
         return x < 0 || x >= map.width ||
-            y < 0 || y >= map.height ||
-            map.imageData.data[(x + y*map.width) * 4 + 1] === 255;
+            y < 50 || y >= map.height ||
+            map.imageData.data[(x + y*map.width) * 4 + 3] === 255;
     }
 }
 
